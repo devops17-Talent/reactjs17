eeb200095d3907387bb3f7a7d30daa45561ce4c7
mallesh <malleshdevops2021@gmail.com>
2023-12-30 04:05:39 +0000

    updated java backend url

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://javaapp.smxgroups.com/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "javaapp.smxgroups.com/api/v1/employees";
 
 class EmployeeService {
 
